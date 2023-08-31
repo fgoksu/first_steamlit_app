f390570d8b87ad0b880e3f95dfd3b2f5f62996e3
Furkan Goksu <47495420+fgoksu@users.noreply.github.com>
2023-08-31 23:19:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,8 @@ streamlit.dataframe(my_data_rows)
 
 
 #ALLOW THE END USER TOA DD A FRUIT TO THE LIST
-add_my_fruit =  streamlit.multiselect("Pick some fruits:", list(my_data_rows.index))
+add_my_fruit =  streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', my_data_rows)
 
 #streamlit.write('Thanks for adding ', add_my_fruit)
 #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
