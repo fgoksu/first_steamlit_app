f06dc201c5a86270aae8046556a5460f42178ec8
Furkan Goksu <47495420+fgoksu@users.noreply.github.com>
2023-08-31 23:50:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,27 +19,28 @@ streamlit.text(' 🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-# Fruityvice api response 
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-    
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-
-if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-else: 
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice);
     fruityvice response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return streamlit.dataframe(fruityvice_normalized) 
 
+#new section to display fruityvice api repsonse
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+else: 
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+    
 except URLError as e:
     streamlit.error()
 
